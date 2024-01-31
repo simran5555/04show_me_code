a7857dd11ce09f7ca7d04f8b4f5c677469236122
simran5555 <simran.kaur@ayasya.com>
2024-01-31 17:43:43 +0530

    login-logout button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,10 @@
-import {LOGO_URL} from "../utils/constants"
+import React from "react";
+import { useState } from "react";
+import {LOGO_URL} from "../utils/constants";
 
 const Header = () => {
+    let btnName = "login"
+    const [btnNameReact, setBtnNameReact] = useState("login")
     return (
         <div className="header">
             <div className="logo-container">
@@ -15,6 +19,12 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button className="loginbtn" type="button" onClick={() => {
+                        btnName = "logout"
+                        btnNameReact==="login"?setBtnNameReact("logout"):setBtnNameReact("login")
+                        console.log(btnName) //the variable is updated but not the UI
+                        console.log(btnNameReact)
+                    }}>{btnNameReact}</button>
                 </ul>
             </div>
         </div>
